cc19ea20938343e9afb4edb8daf42c7f15bb6d42
Michal Kawalec <mkkawalec@gmail.com>
2011-11-06 08:00:33 +0000

    Made a TODO, made audiot actually work.

--- audiot.py
+++ audiot.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python2
+from glob import glob
+from optparse import OptionParser
 
 """
 This is a simple python script to convert music from flac to
@@ -8,8 +10,17 @@ It is Open Source.
 
 
 #A bit of parsing
-from optparse import OptionParser
 parser = OptionParser(usage=__doc__, version='1')
-parser.add_option('-h', '--hosts', dest='HOSTS', default=None, help="Specifies a custom hosts file")
+parser.add_option('-i', '--interactive', action='store_true', dest='INTER', default=False, help='Should the script run in an interactive mode?')
+parser.add_option('-f', '--hosts-file', dest='HOSTS', default=None, help="Specifies a custom hosts file")
 (opts, args) = parser.parse_args()
 
+import fnmatch
+import os
+
+matches = []
+for root, dirnames, filenames in os.walk('/home/sauron/muzyka'):
+  for filename in fnmatch.filter(filenames, '*.mp3'):
+      matches.append(os.path.join(root, filename))
+for match in matches:
+    print match
